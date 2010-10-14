055c885cc2945dbd2179bbb81a3ffe6acf381e40
Evl <wow.evl@gmail.com>
2010-10-14 07:57:43 +0200

    - Update TOC for 4.0 - Remove most of the modules that aren't working in 4.0, the might be added at a later time. Try Dominos, tullaRange and tullaCC

--- evl_BlizzardTweaks.lua
+++ evl_BlizzardTweaks.lua
@@ -2,11 +2,4 @@ local addonName, addon = ...
 
 addon.config = {
 	enhanceTargetFrame = true,
-	hideHotKeyLabels = true,
-	hideMacroLabels = true,
-	compactBars = true,
-	hideShapeShiftBar = false,
-	autoHideMicroMenu = true,
-	autoHideSideBars = true,
-	cooldownThreshold = 0,
 }
